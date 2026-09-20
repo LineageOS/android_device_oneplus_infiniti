3aafdb0ad02704ebb1fcd2d3b836e7a8056003fb
LuK1337 <priv.luk@gmail.com>
2026-09-20 10:53:40 +0200

    infiniti: Set recovery UI density to xxhdpi
    Change-Id: Ie03d4ec80ea7db37b0ff37bed816a254a8de2159

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
+TARGET_RECOVERY_DENSITY := xxhdpi
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
